aed53793135121abb1df38a24081ddd1ecced2b5
aw <aw@a0.ax>
2025-05-19 14:32:24 -0500

    cool

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -40,8 +40,6 @@ declare module '$env/static/private' {
 	export const CUDA_PATH_V12_4: string;
 	export const DriverData: string;
 	export const EFC_5196: string;
-	export const FPS_BROWSER_APP_PROFILE_STRING: string;
-	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const GIT_ASKPASS: string;
 	export const HOME: string;
 	export const HOMEDRIVE: string;
@@ -143,8 +141,6 @@ declare module '$env/dynamic/private' {
 		CUDA_PATH_V12_4: string;
 		DriverData: string;
 		EFC_5196: string;
-		FPS_BROWSER_APP_PROFILE_STRING: string;
-		FPS_BROWSER_USER_PROFILE_STRING: string;
 		GIT_ASKPASS: string;
 		HOME: string;
 		HOMEDRIVE: string;
--- src/lib/components/PostPreview.svelte
+++ src/lib/components/PostPreview.svelte
@@ -1,27 +1,9 @@
 <script>
-  import Card from './Card.svelte'
-  import ArrowRightIcon from './ArrowRightIcon.svelte'
-
-  export let post
+  import Card from './Card.svelte';
+  export let post;
 </script>
 
-<Card href={`/post/${post.slug}`} data-sveltekit-prefetch>
-  <slot slot="eyebrow" name="eyebrow" />
-  <slot slot="title">{post.title}</slot>
-  <div slot="description" class="prose dark:prose-invert">
-    {@html post.preview.html}
-  </div>
-  <div slot="actions">
-    <div class="flex items-center text-teal-500">
-      <span class="text-sm font-medium">Read</span>
-      <ArrowRightIcon class="w-4 h-4 ml-1" />
-    </div>
-  </div>
+<Card>
+  <span slot="title">{post.title}</span>
+  <span slot="date">{@html post.preview.html}</span>
 </Card>
-
-<style>
-  .prose > :global(p) {
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-</style>
